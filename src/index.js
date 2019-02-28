063aec16c2800b61da04ed00088ecd04c7eb212f
Daria Kozielecka <dkozielecka@MacBook-Air-Daria.local>
2019-02-28 13:31:05 +0100

    add board

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import { LifeCounter, lifeCounter } from "./js/LifeCounter.js";
 import { Timer, timer } from "./js/Timer.js";
 import { PointsCounter, pointsCounter } from "./js/PointsCounter.js";
+import { Board, board } from "./js/Board.js";
--- src/js/Board.js
+++ src/js/Board.js
@@ -1,3 +1,16 @@
 class Board {
-  constructor() {}
+  constructor() {
+    this.boardBox = document.querySelector(".board");
+  }
+  makeBox(amount) {
+    for (i = 0; i < amount; i++) {
+      this.itemBox = document.createElement("div");
+      this.itemBox.classList.add("item");
+      this.boardBox.appendChild(this.itemBox);
+    }
+  }
 }
+
+const board = new Board();
+
+export { Board, board };
